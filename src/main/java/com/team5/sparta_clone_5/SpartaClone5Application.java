171b5dceab6fdaed7f2644b80d101a9154d98782
Woozy <anfrosus@naver.com>
2022-10-28 10:25:27 +0900

    init

---
+++ src/main/java/com/team5/sparta_clone_5/SpartaClone5Application.java
@@ -0,0 +1,13 @@
+package com.team5.sparta_clone_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaClone5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaClone5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/team5/sparta_clone_5/SpartaClone5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.sparta_clone_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaClone5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
